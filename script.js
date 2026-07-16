9178c4ba16f800202611d305af11e52da354212b
TriptiGurung117 <gurungtripti2@gmail.com>
2026-07-16 07:27:38 +0545

    Created Expense Tracker

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const amount = document.querySelector("#amount")
+console.log(amount)
+
+const category = document.querySelector("#category")
+const description = document.querySelector("#description")
+
+const tbody = document.querySelector("tbody")
+
+form.addEventListener("submit", (e)=> {
+e.preventDefault();
+
+const row = document.createElement("tr");
+
+
+})
